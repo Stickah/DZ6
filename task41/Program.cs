f3ec61b94446b8f69582344e6b3b6cd4d81e8171
Vladislav <vlad-taranin@yandex.ru>
2022-12-19 12:41:28 +0300

    complete

---
+++ task41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Сколько элементов вы ходтите ввести");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] massiv = new int[M];
+Console.WriteLine("Вводите поочередно элементы");
+for (int i=0; i<M;i++)
+massiv[i] = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int j=0; j<M;j++)
+if (massiv[j]>0)
+count+=1;
+Console.WriteLine("чисел больше нуля "+count);
---
+++ task46/Program.cs
@@ -0,0 +1,9 @@
+﻿double[] massiv = new double[4];
+Console.WriteLine("Вводите поочередно b1,k1,b2,k2");
+for (int i=0; i<4;i++)
+{massiv[i] = Convert.ToInt32(Console.ReadLine());
+}
+double X = (massiv[2]-massiv[0])/(massiv[1]-massiv[3]);
+Console.WriteLine("кордината X точки пересечения "+(X));
+double Y = massiv[1]*X+massiv[0];
+Console.WriteLine("кордината Y точки пересечения "+(Y));
